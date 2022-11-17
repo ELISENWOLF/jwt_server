2e80256a29b2d674d13cc4de14ed06ddd8c72d7c
ELISENWOLF <aravindprakash2001@gmail.com>
2022-11-17 20:17:59 +0530

    Route-study

--- server.js
+++ server.js
@@ -1,49 +1,56 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors');
+const {logger} = require('./middleware/logEvents');
+const errHandler = require('./middleware/errHandler');
 const PORT = process.env.PORT || 3500;
 
-app.get('^/$|/index(.html)?', (req, res) => {
-    // res.sendFile('./views/index.html', { root: __dirname });
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
-});
+//custom middleware logger
+app.use(logger);
 
-app.get('/new-page(.html)?', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
-});
-
-app.get('/old-page(.html)?', (req, res) => {
-    res.redirect(301, '/new-page.html');
-});
-
-//Route Handlers
-app.get('/hello(.html)?', (req, res, next) => {
-    console.log('Attempt to load hello.html');
-    next()
-}, (req, res) => {
-    res.send('Hello World')
-});
-
-//chaining route handlers
-const one = (req, res, next) => {
-    console.log('one');
-    next()
+//Cross Origin Resource Sharing
+const whitelist = ['https://www.myelisen.com', 'http://127.0.0.1:5500', 'http://localhost:3500']
+const corsOptions = {
+    origin: (origin, callback) => {
+        if(whitelist.indexOf(origin) !== -1 || !origin){
+            callback(null, true);
+        }else {
+            callback(new Error('NOT ALLOWED'))
+        }
+    },
+    optionsSuccesStatus: 200
 }
+app.use(cors(corsOptions));
 
-const two = (req, res, next) => {
-    console.log('two');
-    next()
-}
+//built-in middleware to handle urlencoded data
+//in other words, form data:
+// 'content-type: application/x-www-form-urlencoded'
+app.use(express.urlencoded({ extended: false }));
 
-const three = (req, res, next) => {
-    console.log('three');
-    res.send('Finished')
-}
+//built-in middleware for json
+app.use(express.json());
 
-app.get('/chain(.html)?', [one, two, three]);
+//serve static files
+app.use(express.static(path.join(__dirname, '/public')))
+app.use('/subdir', express.static(path.join(__dirname, '/public')))
 
-app.get('/*', (req, res) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-})
+// routes
+app.use('/', require('./routes/root'));
+app.use('/subdir', require('./routes/subdir'));
+app.use('/employees', require('./routes/api/employees'));
+
+app.all('*', (req, res) => {
+    res.status(404);
+    if(req.accepts('html')){
+        res.sendFile(path.join(__dirname, 'views', '404.html'))
+    }else if(req.accepts('json')){
+        res.json({error: "404 not found"});
+    }else {
+        res.type('txt').send('404 not found');
+    }
+});
+
+app.use(errHandler);
 
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
